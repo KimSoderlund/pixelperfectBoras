91867ecf5f49cf06d95762399f7367cd9f4d3802
KimSoderlund <kimsoderlund33@gmail.com>
2026-06-03 12:52:27 +0200

    Added Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Overlay from './components/Overlay'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <>
       <Overlay />
       <Header />
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.scss';
+
+function Hero() {
+  return (
+    <div className="hero-container">
+        <div className="hero-content">
+            <h1>Welcome to the Warhammer Store</h1>
+            <p>Discover the latest releases, exclusive offers, and everything you need to bring your Warhammer battles to life.</p>
+            <button>Shop Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
